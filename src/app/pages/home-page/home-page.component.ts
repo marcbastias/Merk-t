93e19919151101604d9cb1f1655798da3166af9d
Marc <marcbasarr@campus.monlau.com>
2025-07-26 20:20:43 +0200

    Avanzando con el hero-image

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../layouts/header/header.component';
+import { HeroPersonalizationComponent } from '../../shared/components/hero-personalization/hero-personalization.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent, HeroPersonalizationComponent],
   templateUrl: './home-page.component.html',
   standalone: true  
 })
